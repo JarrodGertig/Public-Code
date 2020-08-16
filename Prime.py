7e1d7b06dad19323fbcce30b845d2fe5f2be595c
JarrodGertig <67885291+JarrodGertig@users.noreply.github.com>
2020-08-16 15:11:58 -0600

    Comments
    added comments

--- Prime.py
+++ Prime.py
@@ -1,11 +1,11 @@
 print("enter a number to check if its prime")
-inp = float(input())
-num = int(inp)
-if ((num==inp)and(num > 1)):
-   for i in range(2,num):
-       if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+inp = float(input())                         #Get raw numerical input
+num = int(inp)                               #Turn into integer
+if ((num==inp)and(num > 1)):                 #If float=integer, meaning decimal wasn't input, and its greater than 1
+   for i in range(2,(int(num/2)+1)):         #for range 2 to half the number
+       if (num % i) == 0:                    #if the number is divisible with a 0 zero remainder
+           print(num,"is not a prime number")#its not prime
+           print(i,"times",num//i,"is",num)  #read the factors
            break
    else:
        print(num,"is a prime number")
