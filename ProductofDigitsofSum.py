a77414422a6f142b06003360bb05feff6d972d63
JarrodGertig <67885291+JarrodGertig@users.noreply.github.com>
2020-09-04 15:30:59 -0600

    Update ProductofDigitsofSum.py

--- ProductofDigitsofSum.py
+++ ProductofDigitsofSum.py
@@ -1,24 +1,23 @@
 #Product of Digits of Sum
 #Programmer: Jarrod Gertig
 #Date: Sept. 4, 2020
-#
 
-def funny_func(a,args):
-    for arg in args:
-        a += arg
-    spread = list(str(a))
-    print(spread)
-    beta = 1
-    for letter in spread:
-        beta *= int(letter)
-    while len(str(beta))>1:
-        delta = list(str(beta))
-        print(delta)
-        beta = 1
-        for letter in delta:
-            beta *= int(letter)
-    print(beta)
+#Main math function
+def funny_func(a,args):         # To get the Product of Digits of Sum:
+    for arg in args:            #    for each number
+        a += arg                #      add them all to the first number
+    spread = list(str(a))       #    make a list of digits in the sum
+    beta = 1                    #    multiplier of 1
+    for letter in spread:       #    for each digit    
+        beta *= int(letter)     #       multiply all the digits
+    while len(str(beta))>1:     #    while digit count is > 1
+        delta = list(str(beta)) #       make new list of digits
+        beta = 1                #       multiplier
+        for letter in delta:    #       for each digit
+            beta *= int(letter) #          multiply
+    print(beta)                 #    When only one digit remains, print it.
 
+#Function to get effective inputs
 def injector():
     first = int(input("Enter the first number: "))
     numbers = []
